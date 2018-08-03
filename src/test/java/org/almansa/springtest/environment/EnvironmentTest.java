fae35514c76c5bd10b9a947ed0aba18d6ba6381c
나윤수 <skennel2@gmail.com>
2018-08-03 17:40:12 +0900

    StandardEnvironment

---
+++ src/test/java/org/almansa/springtest/environment/EnvironmentTest.java
@@ -0,0 +1,21 @@
+package org.almansa.springtest.environment;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.core.env.StandardEnvironment;
+
+public class EnvironmentTest {
+	@Test
+	public void systemEnvironmentTest() {		
+		StandardEnvironment env = new StandardEnvironment();
+
+		// 시스템 환경변수 데이터 조회
+		Map<String, Object> se = env.getSystemEnvironment();
+		for (Map.Entry<String, Object> entry : se.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+		
+		System.out.println(env.getProperty("USERPROFILE"));
+	}
+}
