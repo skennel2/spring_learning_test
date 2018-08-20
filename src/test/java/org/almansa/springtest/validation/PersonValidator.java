f6debc3445ddb79793a0475162034f885ffab4d1
나윤수 <skennel2@gmail.com>
2018-08-20 13:50:29 +0900

    BeanWrapper

---
+++ src/test/java/org/almansa/springtest/validation/PersonValidator.java
@@ -0,0 +1,21 @@
+package org.almansa.springtest.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+/**
+ * https://blog.outsider.ne.kr/825
+ * @author skennel
+ *
+ */
+public class PersonValidator implements Validator{
+
+	@Override
+	public boolean supports(Class<?> classType) {		
+		return Person.class.equals(classType);
+	}
+
+	@Override
+	public void validate(Object obj, Errors e) {
+	}
+
+}
