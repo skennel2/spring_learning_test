0563c3c8a34b6ede62ec75bda1c30bb9ac254679
skennel2 <skennel2@gmail.com>
2018-11-29 00:11:39 +0900

    소스정리

--- src/test/java/org/almansa/springtest/core/beanlifecycle/LifeCycleTest.java
+++ src/test/java/org/almansa/springtest/core/beanlifecycle/LifeCycleTest.java
@@ -7,17 +7,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.AbstractApplicationContext;
 
 public class LifeCycleTest {
-    @Test
-    public void applicationContextGetBeanUsingBasePackageTest() {
-        LifeCycleService service = null;
-        try (AbstractApplicationContext context = new AnnotationConfigApplicationContext(
-                "org.almansa.springtest.core.beanlifecycle")) {
-            service = context.getBean(LifeCycleService.class);
-            assertEquals("Initialize", service.getStatus());
-        }
+	@Test
+	public void applicationContextGetBeanUsingBasePackageTest() {
+		LifeCycleService service = null;
+		try (AbstractApplicationContext context = new AnnotationConfigApplicationContext("org.almansa.springtest.core.beanlifecycle")) {
+			service = context.getBean(LifeCycleService.class);
+			assertEquals("Initialize", service.getStatus());
+		}
 
-        // @PreDestroy는 ApplicationContex의 close가 호출된 후에 실행되는 것을 확인할 수 있다.
-        assertEquals("destroy", service.getStatus());
-    }
+		// @PreDestroy는 ApplicationContex의 close가 호출된 후에 실행되는 것을 확인할 수 있다.
+		assertEquals("destroy", service.getStatus());
+	}
 
 }
